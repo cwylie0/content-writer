6ce26270d1b9e64ce47ea2e2b6332df755fa5e9e
cwylie0 <cwylie0@hotmail.com>
2017-11-13 15:07:26 -0500

    First commit

---
+++ contentTemplate.py
@@ -0,0 +1,34 @@
+
+services = {
+	'Diagnostic Service','Glass Repair','Glass & LCD Repair','Battery Replacement','Charging Port Repair','Front Camera Repair','Headphone Jack Repair','Home Button Repair','Loud Speaker Repair','Microphone Repair','Power Button Repair','Rear Camera Repair','Volume Button Repair','Water Damage Repair'
+}
+
+disclaimer = "<p>&nbsp;</p><p><strong>*NOTE: If you decline repairs for diagnostic or water damage treatment or your device is deemed non-repairable, you will be responsible for shipping and handling.</strong></p>"
+
+videoDiv = '<div style="position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden;"><iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: block; margin-left: auto; margin-right: auto;" src="YOUTUBEURL" width="853" height="480" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div><p>&nbsp;</p>'
+
+intro = [
+"<p>&nbsp;</p><p>We understand, your DEVICE is critical. When it breaks, it can really mess up your day. If you are in need of SERVICE, let the professional repair technicians at iFixYouri help you out.</p>",
+"<p>&nbsp;</p><p>Each device has its own lifespan - some devices use higher quality components than others and can last years if properly maintained. And we all know what drops or water can do to electronics. The DEVICE has great build quality and design, but it isn't invincible. Since your device is of little use when broken, it's a good idea to bring it to iFixYouri for expert SERVICE.</p>",
+"<p>&nbsp;</p><p>Doing SERVICE yourself is not recommended since you might unknowingly cause more damage. Using the supplied shipping label, send your DEVICE to iFixYouri's mail-in center so our skilled technicians can inspect each and every component, pinpoint all issues affecting your device, and get them repaired the professional way.</p>"
+]
+
+diagnostic = [
+"<p>&nbsp;</p><p>After checkout, you'll receive an email confirmation and shipping label. After your phone arrives at our repair center, we do diagnostic tests to determine if any other issues are at play. Outgoing tests are performed to ensure that your DEVICE is fully functional. After the repairs are complete, we send you a tracking number and ship your DEVICE back to you!</p>",
+"<p>&nbsp;</p><p>iFixYouri is here to solve your problem with SERVICE for DEVICE. With this service, we'll determine the cause of the problem, get your device repaired, and send it back to you. We'll let you know if there are any other issues affecting your device and give you the option of proceeding with those repairs.</p>",
+"<p>&nbsp;</p><p>Every repair service begins with a series of diagnostic tests to determine what the issue is. Our technicians use their repair experience to quickly diagnose the problem. In most cases, repairs are minimal; however, if we see that your DEVICE has additional damage, we'll contact you before proceeding.</p>"
+]
+
+shipping = [
+"<p>&nbsp;</p><p>With this service, you send your DEVICE to iFixYouri via the UPS shipping method of your choosing. If you opt for standard UPS Ground, we'll pay for the entire cost of shipping and include $100 insurance. Faster shipping and extra insurance coverage can be purchased using the drop-down box above.</p>",
+"<p>&nbsp;</p><p>Faster shipping options and higher tiers of insurance protection are available for purchase.</p>",
+"<p>&nbsp;</p>Free UPS ground shipping and $100 of insurance are included. You can, of course, purchase faster shipping options and higher tiers of insurance protection using the drop-down options above.</p>",
+"<p>&nbsp;</p><p>We send you a shipping label to print, and include $100 of insurance protection. If you would like expedited shipping or more insurance, you have the option to upgrade for an additional fee.</p>"
+]
+
+warranty = [
+"<p>&nbsp;</p><p>If you have specific questions about SERVICE or any DEVICE service from iFixYouri, please ask them in the box below. A 90-day warranty that covers all but accidental damage is included. Contact customer service with any questions at <a href=\"tel:888-494-4349\">888-494-4349</a> or <a href=\"mailto:support@ifixyouri.com\">support@ifixyouri.com</a>.</p>",
+"<p>&nbsp;</p><p>All replacement parts come with a 90-day warranty that covers all but accidental damage. Questions about SERVICE? Type them into the box below, and we'll get back to you as soon as possible. Alternatively, you can reach us at <a href=\"tel:888-494-4349\">888-494-4349</a> or <a href=\"mailto:support@ifixyouri.com\">support@ifixyouri.com</a></p>",
+"<p>&nbsp;</p><p>SERVICE repair includes a 90-day warranty that covers all but accidental damage. If you notice something off about the fit, function, or performance of your replacement part or parts, let us know right away and we will be glad to help. iFixYouri can be reached at <a href=\"tel:888-494-4349\">888-494-4349</a> or <a href=\"mailto:support@ifixyouri.com\">support@ifixyouri.com</a>. Get your DEVICE working like new with the help of experienced technicians who do the job right the first time, and save yourself a lot of money and hassle.</p>"
+]
+
